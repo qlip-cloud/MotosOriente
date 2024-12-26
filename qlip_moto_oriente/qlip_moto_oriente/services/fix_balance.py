e06a2e85576e201af79c7fd1868c277c0f2b13e6
HendersonMentum <henderson.villegas@mentum.group>
2024-12-26 22:54:21 +0000

    add changes to fix balance on sales invoices

--- qlip_moto_oriente/qlip_moto_oriente/services/fix_balance.py
+++ qlip_moto_oriente/qlip_moto_oriente/services/fix_balance.py
@@ -70,12 +70,12 @@ def exec():
             parent = row.parent
             amount = row.gle_entry_total_without_outstanding
         
-        if row.payment_amount <= amount:
+        if row.payment_amount <= amount and amount > 0:
             row.paid_amount = row.payment_amount
             amount -= row.payment_amount
             row.outstanding = 0
             
-        if row.payment_amount > amount:
+        if row.payment_amount > amount and amount > 0:
             row.paid_amount = amount
             row.outstanding = row.payment_amount - amount
             amount -= amount
